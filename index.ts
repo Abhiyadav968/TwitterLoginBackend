bf29f0cf3d32bf01a8c9a425febae9b298834fd4
abhiSuvaidyam <abhi.suvaidyam@gmail.com>
2023-09-26 11:55:15 +0530

    add

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
-
 const PORT = 3000;
 const path = require('path');
-const Cors = require('cors');
+const cors = require('cors');
 const passport = require('passport');
 const express = require('express');
 const app = express();
 const Strategy = require('passport-twitter').Strategy;
 const session = require('express-session');
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = 'nnhduiewhfkjenewjkbewuifwefiwwvbwiufewfaksniudhwedjckd';
 require('dotenv').config();
 
+app.use(cors());
 
 passport.use(new Strategy({
     consumerKey: process.env.TWITTER_CONSUMER_KEY,
@@ -30,7 +32,6 @@ passport.deserializeUser(function (obj: any, callback: any) {
     callback(null, obj);
 });
 
-app.use(Cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client')));
@@ -47,7 +48,8 @@ app.get('/profile', (req: any, res: any) => {
     const user = req.user;
     const name = user.displayName;
     const profileImageUrl = user.photos[0].value;
-    res.send(`Name ${name} <br> <br> <img src="${profileImageUrl}" alt="User Profile Image">  <br> <br> `);
+    res.send(`Name ${name} <br> <br> <img src="${profileImageUrl}" alt="User Profile Image"> <br><br> `);
+    // console.log(req.query.token)
 });
 
 app.get('/twitter/login', passport.authenticate('twitter'));
@@ -55,9 +57,11 @@ app.get('/twitter/login', passport.authenticate('twitter'));
 app.get('/twitter/return', passport.authenticate('twitter', {
     failureRedirect: '/'
 }), function (req: any, res: any) {
-    res.redirect('/profile')
+    const token = jwt.sign({ username: req.user.username, name: req.user.displayName, id: req.user.id }, SECRET_KEY);
+    console.log(token)
+    return res.redirect(`/profile?token=${token}`); // 
 });
 
 app.listen(PORT, () => {
     console.log(`app is running on port ${PORT}`);
-});
+}); 
